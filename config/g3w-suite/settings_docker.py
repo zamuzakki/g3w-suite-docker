f91c927e28d0ede4ea4417393326d59e29c47bda
zamuzakki <zakki@kartoza.com>
2024-03-20 11:43:14 +0700

    Add QPS time series to installed apps

--- config/g3w-suite/settings_docker.py
+++ config/g3w-suite/settings_docker.py
@@ -24,7 +24,8 @@ G3WADMIN_LOCAL_MORE_APPS = [
     # Uncomment if you wont activate the following module
     #'openrouteservice',
     'qtimeseries',
-    'frontend'
+    'frontend',
+    'qps_timeseries'
 ]
 
 DATABASES = {
@@ -209,6 +210,5 @@ LOGGING = {
 
 SESSION_COOKIE_NAME = 'gis3w-suite-dev-iehtgdb264t5gr'
 
-if str(os.getenv('FRONTEND')).lower() in ['true', 'yes', '1']:
-    FRONTEND = True
-    FRONTEND_APP = 'frontend'
+FRONTEND = True
+FRONTEND_APP = 'frontend'
